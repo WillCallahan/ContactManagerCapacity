def25d3026290ca164f71d464663571c8174cd7e
William Callahan <willcallahan94@att.net>
2018-01-11 22:30:12 -0500

    Support for running API requests in Parallel

--- ContactManagerCapacity/Program.fs
+++ ContactManagerCapacity/Program.fs
@@ -10,30 +10,34 @@ module internal Main =
     open ContactManagerCapacity.Main.SNS
 
     [<Literal>]
-    let ContactsToAdd = 10
+    let ContactsToAdd = 1000
 
     [<Literal>]
-    let CapacityNotificationLimit = 1
-
-    let personApiRepository = new PersonApiRepository()
+    let CapacityNotificationLimit = 100
 
     let addContact (i : int, notifyAt : int) =
-        i |> Person.Seed |> personApiRepository.save |> JsonSerializer.encode |> printfn "Added person %s"
+        let personApiRepository = new PersonApiRepository()
+        i |> Person.Seed |> personApiRepository.save |> JsonSerializer.encode |> printfn "Added person %d \r\n\r\n\t%s\r\n" i
         match i with
             | i when i <> 0 && i % notifyAt = 0 -> (personApiRepository.findAll
                                                         |> JsonSerializer.encode
                                                         |> SNSNotificationService.notify 
                                                         |> ignore)
+            | _ -> ()
         ()
 
     let addContacts (total : int, notifyAt : int) =
-        printfn "Adding %d contacts" total
-        [1..total] |> List.iter (fun i -> addContact(i, notifyAt))
-        printfn "Added all contacts"
+        printfn "Adding %d contacts\r\n%0*d" total 80 0
+        seq [ for i in 1..total -> async { addContact(i, notifyAt) |> ignore }; ]
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> ignore
+        printfn "%0*d\r\nAdded all contacts" 80 0
 
     [<EntryPoint>]
     let main argv = 
-        printfn "Contact Capacity Program"
+        printfn "Contact Capacity Program\r\n"
         addContacts(ContactsToAdd, CapacityNotificationLimit) |> ignore
+        printfn "\r\nPress any key to exit..."
         Console.ReadKey() |> ignore
         0
